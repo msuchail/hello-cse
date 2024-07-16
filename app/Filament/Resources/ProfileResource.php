2d2f74d0fbf425b90d43ecbb66384be9dbb56910
Mathias SUCHAIL <mathias.suchail@yahoo.com>
2024-07-16 14:25:19 +0200

    :rocket: Back office fonctionnel

---
+++ app/Filament/Resources/ProfileResource.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProfileResource\Pages;
+use App\Filament\Resources\ProfileResource\RelationManagers;
+use App\Models\Profile;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteAction;
+use Filament\Actions\RestoreAction;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProfileResource extends Resource
+{
+    protected static ?string $model = Profile::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('statut')
+                    ->options([
+                        'active' => 'Actif',
+                        'inactive' => 'Inactif',
+                        'pending' => 'En attente',
+                    ])
+                    ->default('pending')
+                    ->columnSpan(2),
+                Forms\Components\TextInput::make('nom')
+                    ->nullable(false),
+                Forms\Components\TextInput::make('prenom')
+                    ->nullable(false),
+                Forms\Components\FileUpload::make('image')
+                    ->image()
+                    ->columnSpan(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nom')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('prenom')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('statut')
+                    ->icon(fn (string $state): string => match ($state) {
+                        'active' => 'heroicon-m-check-circle',
+                        'inactive' => 'heroicon-m-x-mark',
+                        'pending' => 'heroicon-m-clock',
+                    })->color(fn($state): string => match ($state) {
+                        'active' => 'success',
+                        'inactive' => 'danger',
+                        'pending' => 'warning',
+                    })
+                    ->sortable(),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ForceDeleteAction::make(),
+                Tables\Actions\RestoreAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make()
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProfiles::route('/'),
+            'create' => Pages\CreateProfile::route('/create'),
+            'edit' => Pages\EditProfile::route('/{record}/edit'),
+        ];
+    }
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
